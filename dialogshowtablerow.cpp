b31f8f37857c421107198ef7f6d5473682c1c0e8
幻舞奇影 <793554262@qq.com>
2018-04-12 21:38:13 +0800

    第一版打开详情开发完毕

--- dialogshowtablerow.cpp
+++ dialogshowtablerow.cpp
@@ -6,20 +6,17 @@ DialogShowTableRow::DialogShowTableRow(QList<QStringList> * rowdata,QWidget *par
     ui(new Ui::DialogShowTableRow)
 {
     ui->setupUi(this);
+    this->setWindowFlags(Qt::Dialog | Qt::WindowCloseButtonHint);
 
     //初始化表格
     QTableWidget * ptr_table =ui->tableWidget;
     ptr_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ptr_table->setSelectionBehavior(QAbstractItemView::SelectRows);
     ptr_table->setColumnCount(4);
-    int width=ptr_table->width()-20;
-    ptr_table->setColumnWidth(0,(width/4)-1);
-    ptr_table->setColumnWidth(1,(width/4)-1);
-    ptr_table->setColumnWidth(2,(width/4)-1);
-    ptr_table->setColumnWidth(3,(width/4)-1);
     ptr_table->setRowCount(rowdata->count());
     ptr_table->setAlternatingRowColors(true);
     ptr_table->verticalHeader()->setDefaultSectionSize(22);
+    ptr_table->horizontalHeader()->setStretchLastSection(true);//关键
     //设置表格列标题
     QStringList title;
     title.append("字段中文名");
@@ -36,6 +33,7 @@ DialogShowTableRow::DialogShowTableRow(QList<QStringList> * rowdata,QWidget *par
             ptr_table->setItem(row, col, item);
         }
     }
+    ptr_table->resizeColumnsToContents();
 }
 
 DialogShowTableRow::~DialogShowTableRow()
