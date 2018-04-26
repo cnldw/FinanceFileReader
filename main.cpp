b690213897557fcb8fca88e702b15024d7bd95b2
幻舞奇影 <793554262@qq.com>
2018-04-26 17:23:47 +0800

    启用高分辨率支持

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <QTranslator>
 int main(int argc, char *argv[])
 {
+    //启用高分辨率支持
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication app(argc, argv);
     QTranslator translator;
     //加载翻译
