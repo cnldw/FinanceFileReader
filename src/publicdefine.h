3ea08c2cf3bd36b36b597cb97d64d34e73904785
幻舞奇影 <793554262@qq.com>
2019-12-21 22:34:11 +0800

    1.8.2版本新增开放式基金交换协议2.2版本解析支持

--- src/publicdefine.h
+++ src/publicdefine.h
@@ -16,7 +16,7 @@
 #ifndef PUBLICDEFINE_H
 #define PUBLICDEFINE_H
 
-#define VERSION_V "V1.8.1"
+#define VERSION_V "V1.8.2"
 #define AUTHOR_PHONE "MTUyMzg4NzIxMDE="
 #define AUTHOR_NAME "NzkzNTU0MjYyQHFxLmNvbSjliJjlvrfkvY0p"
 #define AUTHOR_EMAIL "NzkzNTU0MjYyQHFxLmNvbQ=="
